7017be918e23477cb3ae1175d4e4ae76f09305c7
Demetrius Bagas Primanto <39642138+bagasprimanto@users.noreply.github.com>
2025-03-16 12:13:31 +0700

    Add tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import { load as home } from "./home.js"
 import { load as menu } from "./menu.js"
 
+const btnHome = document.querySelector(".btn.nav-home");
+const btnMenu = document.querySelector(".btn.nav-menu");
+const btnContactUs = document.querySelector(".btn.nav-contactUs");
+const content = document.querySelector("div#content");
+
+btnHome.addEventListener("click", loadHome);
+btnMenu.addEventListener("click", loadMenu);
+
+function clearContent() {
+    content.innerHTML = "";
+}
+
+function loadHome() {
+    clearContent();
+    home();
+}
+
+function loadMenu() {
+    clearContent();
+    menu();
+}
+
 // Call home page load
-// home();
-menu();
+home();
+// menu();
 console.log("Hello world!");
